77febacec122ed0de0d50fdbacf78a5356bf8a08
vijay <vijay.dce93@gmail.com>
2024-05-31 05:32:08 +0530

    Route structure

--- main.js
+++ main.js
@@ -8,6 +8,21 @@ app.get("/", (req, res) => {
   res.json({ msg: "Welcome Students!" });
 });
 
+//CRUD Functionality of move
+
+//For Reading movies
+app.get("/movies", () => {});
+
+//For Creating movies
+app.post("/movies", () => {});
+
+//For Updating movie
+app.put("/movies/:id", () => {});
+
+//For Deleting movie
+
+app.delete("/movies/:id", () => {});
+
 app.listen(PORT, () => {
   console.log(`the server is running at http://localhost:${PORT}`);
 });
